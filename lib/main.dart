103dfefbe90643d716936ae7ca17667da39774ed
NaitoFumi <akinorifumi@gmail.com>
2023-02-15 22:07:31 +0900

    modify Calender function
    Display of daily tasks
    Change of format

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
-import 'package:flutter_calendar_carousel/classes/event.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,34 +24,184 @@ class _CalendarScreenState extends State<CalendarScreen> {
 
   // This function is used to listen to the date changes.
   void onSelectedDate(DateTime dateTime) {}
+  DateTime _focusedDay = DateTime.now();
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime? _selectedDay;
+  Map<DateTime, List> _eventsList = {};
+
+  // Data fields
+  List<String> events = ["Event 1", "Event 2"];
+  List<String> parts = ["Part 1", "Part 2"];
+
+  // Form fields
+  String selectedEvent = "Event 1";
+  String selectedPart = "Part 1";
+
+  //event loader
+  int getHashCode(DateTime key) {
+    return key.day * 1000000 + key.month * 10000 + key.year;
+  }
+  @override
+  //event loader
+  void initState() {
+    super.initState();
+    _selectedDay = _focusedDay;
+    //サンプルのイベントリスト
+    _eventsList = {
+      DateTime.now().subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
+      DateTime.now(): ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
+      DateTime.now().add(Duration(days: 1)): [
+        'Event A8',
+        'Event B8',
+        'Event C8',
+        'Event D8'
+      ],
+      DateTime.now().add(Duration(days: 3)):
+          Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
+      DateTime.now().add(Duration(days: 7)): [
+        'Event A10',
+        'Event B10',
+        'Event C10'
+      ],
+      DateTime.now().add(Duration(days: 11)): ['Event A11', 'Event B11'],
+      DateTime.now().add(Duration(days: 17)): [
+        'Event A12',
+        'Event B12',
+        'Event C12',
+        'Event D12'
+      ],
+      DateTime.now().add(Duration(days: 22)): ['Event A13', 'Event B13'],
+      DateTime.now().add(Duration(days: 26)): [
+        'Event A14',
+        'Event B14',
+        'Event C14'
+      ],
+    };
+  }
 
   @override
   Widget build(BuildContext context) {
+    final _events = LinkedHashMap<DateTime, List>(
+      equals: isSameDay,
+      hashCode: getHashCode,
+    )..addAll(_eventsList);
+
+    List _getEventForDay(DateTime day) {
+      return _events[day] ?? [];
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text("Calendar"),
       ),
-      body: Container(
-          margin: const EdgeInsets.only(top:10, left:30, right:30),
-          child: CalendarCarousel<Event>(
-            //アイコンを表示する日付について、EventのList
-            markedDateShowIcon: true,
-            markedDateIconMaxShown: 1,
-            markedDateMoreShowTotal: null,
-            markedDateIconBuilder: (event)=>event.icon,  //アイコン
-            onDayPressed: (DateTime date, List<Event> events) {
-              // this.setState(() => _currentDate = date);
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => TrainingRecordScreen()),
-              );
-            },
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            child:
+              TableCalendar(
+                // 以下必ず設定が必要
+                firstDay: DateTime.utc(2020, 1, 1),
+                lastDay: DateTime.utc(2030, 12, 31),
+                focusedDay: _focusedDay,
+                calendarFormat: _calendarFormat,
+                onFormatChanged: (format) {
+                  if (_calendarFormat != format) {
+                    setState(() {
+                      _calendarFormat = format;
+                    });
+                  }
+                },
+                selectedDayPredicate: (day) {
+                  return isSameDay(_selectedDay, day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  if (!isSameDay(_selectedDay, selectedDay)) {
+                    setState(() {
+                      _selectedDay = selectedDay;
+                      _focusedDay = focusedDay;
+                    });
+                  }
+                },
+                eventLoader: _getEventForDay,
+              ),
           ),
+          ListView(
+            shrinkWrap: true,
+            children: _getEventForDay(_selectedDay!)
+                .map((event) => ListTile(
+                      title: Text(event.toString()),
+                    ))
+                .toList(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: DropdownButtonFormField(
+                    // Populate list of events
+                    items: events.map((event) {
+                      return DropdownMenuItem(
+                        value: event,
+                        child: Text(event)
+                      );
+                    }).toList(),
+                    // Set the value
+                    value: selectedEvent,
+                    onChanged: (String? value) {
+                      setState(() {
+                        selectedEvent = value!;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(width: 16),
+                Expanded(
+                  child: DropdownButtonFormField(
+                    // Populate list of parts
+                    items: parts.map((part) {
+                      return DropdownMenuItem(
+                        value: part,
+                        child: Text(part)
+                      );
+                    }).toList(),
+                    // Set the value
+                    value: selectedPart,
+                    onChanged: (String? value) {
+                      setState(() {
+                        selectedPart = value!;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(width: 16),
+                Expanded(
+                  child: DropdownButtonFormField(
+                    // Populate list of parts
+                    items: parts.map((part) {
+                      return DropdownMenuItem(
+                        value: part,
+                        child: Text(part)
+                      );
+                    }).toList(),
+                    // Set the value
+                    value: selectedPart,
+                    onChanged: (String? value) {
+                      setState(() {
+                        selectedPart = value!;
+                      });
+                  },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]
       )
     );
   }
 }
-
 class TrainingRecordScreen extends StatefulWidget {
   const TrainingRecordScreen({super.key});
 
