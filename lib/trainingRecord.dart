3373402b07c00868267b24aca02a3393b3915d58
NaitoFumi <akinorifumi@gmail.com>
2023-03-10 23:05:04 +0900

    modify Widget Componentization

--- lib/trainingRecord.dart
+++ lib/trainingRecord.dart
@@ -3,11 +3,12 @@ import 'package:flutter/services.dart';
 
 import './logger_wrap.dart';
 import './trainingDb.dart';
+import './utilWidget.dart';
 
 // ignore: must_be_immutable
 class TrainingRecordScreen extends StatefulWidget {
   // ignore: prefer_typing_uninitialized_variables
-  var paramDate;
+  final paramDate;
 
   TrainingRecordScreen({Key? key, required this.paramDate}) : super(key: key);
 
@@ -16,12 +17,11 @@ class TrainingRecordScreen extends StatefulWidget {
   _TrainingRecordScreenState createState() => _TrainingRecordScreenState();
 }
 class _TrainingRecordScreenState extends State<TrainingRecordScreen> {
+
   final dbHelper = TrainingDatabase.instance;
-  TextEditingController _weightTtextController = TextEditingController();
-  TextEditingController _repsTtextController = TextEditingController();
-  TextEditingController _lapTtextController = TextEditingController();
-  TextEditingController _metsTtextController = TextEditingController();
+
   TextEditingController _bodyWeightTtextController = TextEditingController();
+
   // Data fields
   List<Evnet> events = [];
   void _getEventList() async {
@@ -31,74 +31,46 @@ class _TrainingRecordScreenState extends State<TrainingRecordScreen> {
       logger.d(value.name);
     }
   }
-  double weight = 0;
-  int reps = 0;
-  double rm = 0;
-  double calculateRM(double weight, int reps) {
-    return (weight * (1 + (reps / 30)));
+
+  List<Widget> _items = <Widget>[];
+  List<Widget> _list = <Widget>[];
+  void addWidgetList() {
+    logger.d(_list);
+    _list.add(
+      TrainingSetFormTextList()
+    );
+    logger.d(_list);
+    setState(() {
+      _items = _list;
+    });
   }
+
   int lap = 0;
   int interval = 0;
   double mets = 0;
   double kcal = 0;
   double bodyWeight = 0;
+
   double calculateKcal(int lap, double bodyWeight, double mets) {
     return (mets * bodyWeight * (lap / 360) * 1.05);
   }
 
   int selectedEvent = 1;
   late DateTime localDate;
+  String a = "a";
 
   @override
   void initState() {
     super.initState();
     localDate = widget.paramDate;
     _getEventList();
-    _weightTtextController.addListener(_updateWeight);
-    _repsTtextController.addListener(_updateReps);
-    _lapTtextController.addListener(_updateLap);
-    _metsTtextController.addListener(_updateMets);
-    _bodyWeightTtextController.addListener(_updateBodyWeight);
   }
-  void _updateWeight() {
-    setState(() {
-      weight = double.parse(_weightTtextController.text);
-      rm = calculateRM(weight, reps);
-    });
-  }
-  void _updateReps() {
-    setState(() {
-      reps = int.parse(_repsTtextController.text);
-      rm = calculateRM(weight, reps);
-    });
-  }
-  void _updateLap() {
-    setState(() {
-      lap = int.parse(_lapTtextController.text);
-      kcal = calculateKcal(lap, bodyWeight, mets);
-    });
-  }
-  void _updateMets(){
-    setState(() {
-      mets = double.parse(_metsTtextController.text);
-      kcal = calculateKcal(lap, bodyWeight, mets);
-    });
-  }
+
   void _updateBodyWeight() {
     setState(() {
       bodyWeight = double.parse(_bodyWeightTtextController.text);
       kcal = calculateKcal(lap, bodyWeight, mets);
     });
-  }
-
-  @override
-  void dispose() {
-    _weightTtextController.dispose();
-    _repsTtextController.dispose();
-    _lapTtextController.dispose();
-    _metsTtextController.dispose();
-    _bodyWeightTtextController.dispose();
-    super.dispose();
   }
 
   @override
@@ -158,185 +130,18 @@ class _TrainingRecordScreenState extends State<TrainingRecordScreen> {
                 ],
               ),
             SizedBox(height: 16.0),
-            Column(
-              children: <Widget>[
-                Row( //weight reps
-                  children: [
-                    Expanded( //weight
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Expanded(
-                            child: TextFormField(
-                              keyboardType: TextInputType.number,
-                              controller: _weightTtextController,
-                              inputFormatters: <TextInputFormatter>[
-                                FilteringTextInputFormatter.digitsOnly // Only numbers can be entered
-                              ],
-                              decoration: const InputDecoration(
-                                hintText: 'Enter a number',
-                                border: OutlineInputBorder(),
-                                labelText: "Weight",
-                                suffix: Text('kg'),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Expanded( //reps
-                      child:Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children:<Widget>[
-                          Expanded(
-                            child:TextFormField(
-                              keyboardType: TextInputType.number,
-                              controller: _repsTtextController,
-                              inputFormatters: <TextInputFormatter>[
-                                FilteringTextInputFormatter.digitsOnly // Only numbers can be entered
-                              ],
-                              decoration:const InputDecoration(
-                                hintText: 'Enter a number',
-                                border: OutlineInputBorder(),
-                                labelText: "Reps",
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ]
-                ),
-                Row( //lap interval mets
-                  children: [
-                    Expanded( //lap
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Expanded(
-                            child: TextFormField(
-                              keyboardType: TextInputType.number,
-                              controller: _lapTtextController,
-                              inputFormatters: <TextInputFormatter>[
-                                FilteringTextInputFormatter.digitsOnly // Only numbers can be entered
-                              ],
-                              decoration: const InputDecoration(
-                                hintText: 'Enter a time',
-                                border: OutlineInputBorder(),
-                                labelText: "Lap",
-                              ),
-                            ),
-                          )
-                        ]
-                      ),
-                    ),
-                    // Expanded( //interval
-                    //   child: Row(
-                    //     mainAxisAlignment: MainAxisAlignment.center,
-                    //     children: <Widget>[
-                    //       Expanded(
-                    //         child: TextFormField(
-                    //           keyboardType: TextInputType.number,
-                    //           inputFormatters: <TextInputFormatter>[
-                    //             FilteringTextInputFormatter.digitsOnly // Only numbers can be entered
-                    //           ],
-                    //           decoration: const InputDecoration(
-                    //             hintText: 'Enter a time',
-                    //             border: OutlineInputBorder(),
-                    //             labelText: "Interval",
-                    //           ),
-                    //         ),
-                    //       )
-                    //     ]
-                    //   ),
-                    // ),
-                    Expanded( //mets
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Expanded(
-                            child: TextFormField(
-                              keyboardType: TextInputType.number,
-                              controller: _metsTtextController,
-                              inputFormatters: <TextInputFormatter>[
-                                FilteringTextInputFormatter.digitsOnly // Only numbers can be entered
-                              ],
-                              decoration: const InputDecoration(
-                                hintText: 'Enter a time',
-                                border: OutlineInputBorder(),
-                                labelText: "mets",
-                              ),
-                            ),
-                          )
-                        ]
-                      ),
-                    ),
-                  ],
-                ),
-                Column( //rm
-                  children: [
-                    Text(
-                      '1RM = ${rm.toStringAsFixed(1)} kg',
-                      style: TextStyle(fontSize: 24.0),
-                    ),
-                    SizedBox(height: 16.0),
-                    Text(
-                      'Formula: ${weight} kg x (1 + ${reps} / 30)',
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                  ],
-                ),
-                Column( //kcal
-                  children: [
-                    Text(
-                      '${kcal.toStringAsFixed(1)} kcal',
-                      style: TextStyle(fontSize: 24.0),
-                    ),
-                    SizedBox(height: 16.0),
-                    Text(
-                      'Formula: ${mets} x ${bodyWeight} * ( ${lap} / 360 ) * 1.05',
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                  ],
-                ),
-              ]
+            ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemBuilder: (BuildContext context, int index){
+                return _items[index];
+              },
+              itemCount: _items.length,
             ),
+            SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () async {
-                // handle add training data button tap here
-                // create a new TrainingTask object
-                TrainingTask task = TrainingTask(
-                  date: localDate,
-                  eventId: selectedEvent
-                );
-                int taskId = await dbHelper.insertTrainingTask(task);
-                // check if the insert was successful
-                if (taskId > 0) {
-                  logger.d('TrainingTask inserted with taskId: $taskId');
-                } else {
-                  logger.d('Failed to insert TrainingTask');
-                  // ignore: use_build_context_synchronously
-                  Navigator.pop(context);
-                }
-                TrainingSet set = TrainingSet(
-                  trainingTaskId: taskId,
-                  weight: weight.toDouble(),
-                  reps: reps,
-                  lapTime: 10,
-                  intervalTime: 10,
-                  mets: 5.0,
-                  kcal: 100,
-                  rm: 10,
-                );
-                int setId = await dbHelper.insertTrainingSet(set);
-                // check if the insert was successful
-                if (setId > 0) {
-                  logger.d('TrainingSet inserted with setId: $setId');
-                } else {
-                  logger.d('Failed to insert TrainingSet');
-                  Navigator.pop(context);
-                }
-                Navigator.pop(context);
+                addWidgetList();
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.blue,
@@ -349,6 +154,66 @@ class _TrainingRecordScreenState extends State<TrainingRecordScreen> {
                 mainAxisSize: MainAxisSize.min,
                 children: const [
                   Icon(Icons.add, color: Colors.white),
+                  SizedBox(width: 4),
+                  Text(
+                    "Add",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () async {
+                // handle add training data button tap here
+                // create a new TrainingTask object
+                // TrainingTask task = TrainingTask(
+                //   date: localDate,
+                //   eventId: selectedEvent
+                // );
+                // int taskId = await dbHelper.insertTrainingTask(task);
+                // // check if the insert was successful
+                // if (taskId > 0) {
+                //   logger.d('TrainingTask inserted with taskId: $taskId');
+                // } else {
+                //   logger.d('Failed to insert TrainingTask');
+                //   // ignore: use_build_context_synchronously
+                //   Navigator.pop(context);
+                // }
+                // TrainingSet set = TrainingSet(
+                //   trainingTaskId: taskId,
+                //   weight: weight.toDouble(),
+                //   reps: reps,
+                //   lapTime: 10,
+                //   intervalTime: 10,
+                //   mets: 5.0,
+                //   kcal: 100,
+                //   rm: 10,
+                // );
+                // int setId = await dbHelper.insertTrainingSet(set);
+                // // check if the insert was successful
+                // if (setId > 0) {
+                //   logger.d('TrainingSet inserted with setId: $setId');
+                // } else {
+                //   logger.d('Failed to insert TrainingSet');
+                //   Navigator.pop(context);
+                // }
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24),
+                ),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
                   SizedBox(width: 4),
                   Text(
                     "Regist",
--- lib/utilWidget.dart
+++ lib/utilWidget.dart
@@ -1,4 +1,188 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 
 import './logger_wrap.dart';
 
+class TrainingSetFormText extends StatelessWidget {
+
+  final TextEditingController textController;
+  final String label;
+  final String hint;
+
+  TrainingSetFormText(
+    {
+      Key? key,
+      required this.textController,
+      required this.label,
+      required this.hint,
+    }
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return
+    Expanded( //reps
+      child:Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:<Widget>[
+          Expanded(
+            child:TextFormField(
+              keyboardType: TextInputType.number,
+              controller: textController,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly // Only numbers can be entered
+              ],
+              decoration: InputDecoration(
+                labelText: label,
+                hintText: hint,
+                border: OutlineInputBorder(),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TrainingSetFormTextList extends StatefulWidget {
+
+  TrainingSetFormTextList(
+    {
+      Key? key,
+    }
+  );
+
+  @override
+  _TrainingSetFormTextList createState() => _TrainingSetFormTextList();
+
+}
+
+class _TrainingSetFormTextList extends State<TrainingSetFormTextList> {
+
+  TextEditingController _weightTtextController = TextEditingController();
+  TextEditingController _repsTtextController = TextEditingController();
+  TextEditingController _lapTtextController = TextEditingController();
+  TextEditingController _metsTtextController = TextEditingController();
+  TextEditingController _bodyWeightTtextController = TextEditingController();
+
+  double weight = 0;
+  int reps = 0;
+  double rm = 0;
+
+  double calculateRM(double weight, int reps) {
+    double rm = 0;
+    if(reps == 1){
+      rm = weight;
+    }
+    else {
+     rm = (weight * (1 + (reps / 40)));
+    }
+    return rm;
+  }
+
+  int lap = 0;
+  int interval = 0;
+  double mets = 0;
+  double bodyWeight = 0;
+
+  double calculateKcal(int lap, double bodyWeight, double mets) {
+    return (mets * bodyWeight * (lap / 360) * 1.05);
+  }
+
+  void _updateWeight() {
+    if(_weightTtextController.text.isNotEmpty){
+      setState(() {
+        weight = double.parse(_weightTtextController.text);
+        rm = calculateRM(weight, reps);
+      });
+    }
+  }
+  void _updateReps() {
+    if(_repsTtextController.text.isNotEmpty) {
+      setState(() {
+        reps = int.parse(_repsTtextController.text);
+        rm = calculateRM(weight, reps);
+      });
+    }
+  }
+  void _updateLap() {
+    if(_lapTtextController.text.isNotEmpty) {
+      lap = int.parse(_lapTtextController.text);
+    }
+  }
+  void _updateMets(){
+    if(_metsTtextController.text.isNotEmpty) {
+      mets = double.parse(_metsTtextController.text);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _weightTtextController.addListener(_updateWeight);
+    _repsTtextController.addListener(_updateReps);
+    _lapTtextController.addListener(_updateLap);
+    _metsTtextController.addListener(_updateMets);
+  }
+
+  @override
+  void dispose() {
+    _weightTtextController.dispose();
+    _repsTtextController.dispose();
+    _lapTtextController.dispose();
+    _metsTtextController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return
+    Column(
+      children: <Widget>[
+        Row( //weight reps
+          children: [
+            TrainingSetFormText(
+              textController:_weightTtextController,
+              label: 'Reps',
+              hint: 'Enter a number',
+            ),
+            TrainingSetFormText(
+              textController:_repsTtextController,
+              label: 'Weight',
+              hint: 'Enter a number',
+            ),
+          ]
+        ),
+        Row( //lap interval mets
+          children: [
+            TrainingSetFormText(
+              textController:_lapTtextController,
+              label: 'Lap',
+              hint: 'Enter a time',
+            ),
+            // TrainingSetFormText(
+            //   textController:_metsTtextController,
+            //   label: 'Interval',
+            //   hint: 'Enter a time',
+            // ),
+            TrainingSetFormText(
+              textController:_metsTtextController,
+              label: 'Mets',
+              hint: 'Enter a number',
+            ),
+          ],
+        ),
+        Column( //rm
+          children: [
+            Text(
+              '1RM = ${rm.toStringAsFixed(1)} kg',
+              style: TextStyle(fontSize: 24.0),
+            ),
+          ],
+        ),
+      ]
+    );
+  }
+}
